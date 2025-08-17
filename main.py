8a2d8414025f2f2fc5122e8a7e4719efadb9ebf3
edwingoed13 <111406882+edwingoed13@users.noreply.github.com>
2025-08-17 17:31:55 -0500

    Update2 main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, Response
 import httpx
 import asyncio
 import os
